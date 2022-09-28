fc47aaaf7a4f371eb04ebca7fa9b7fdbb4056fe2
Samir Rodrigues <emaildesamir@gmail.com>
2022-09-28 11:55:56 -0300

    Base do código + commportamento de agrupar com aliados

---
+++ entity.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Entity
+{
+    public int x;
+    public int y;
+
+    public void Set(int newX, int newY)
+    {
+        x = newX;
+        y = newY;
+    }
+
+}
